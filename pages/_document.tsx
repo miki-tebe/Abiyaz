6e7b0387b82efbf0ce16ec37fcec45009b6d7687
Mikiyas Tibebu <mikitebe123@gmail.com>
2022-03-21 15:44:12 +0300

    install next-pwa and setup pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,76 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="Abiyaz" content="Abiyaz" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Abiyaz" />
+          <meta name="description" content="Ethiopian Albums" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+
+          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/icons/touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/icons/touch-icon-ipad-retina.png"
+          />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://abiyaz.vercel.app" />
+          <meta name="twitter:title" content="Abiyaz" />
+          <meta name="twitter:description" content="Ethiopian Albums" />
+          <meta
+            name="twitter:image"
+            content="https://abiyaz.vercel.app/icons/android-chrome-192x192.png"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Abiyaz" />
+          <meta property="og:description" content="Ethiopian Albums" />
+          <meta property="og:site_name" content="Abiyaz" />
+          <meta property="og:url" content="https://abiyaz.vercel.app" />
+          <meta
+            property="og:image"
+            content="https://abiyaz.vercel.app/icons/apple-touch-icon.png"
+          />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
